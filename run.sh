8a527e43c79d7dd9027ee097b7664c76aba75f9b
Brlndtech <brlndtech@gmail.com>
2021-03-23 11:57:49 +0100

    algorithm optimization for logs output-* (!= between host is unreachable and request timed out is ok)

--- run.sh
+++ run.sh
@@ -5,18 +5,17 @@ d="$(date +%d-%m-%Y-%H-%M)"
 lines=$(cat $file)
 for ip in $lines ; 
 do (  ##### sub shell who allow us to execute parallel pings (thread)
-      ping $ip -c1 &> /dev/null ; 
-      if [ $? -eq 0 ]; 
+      if ping -c 1 $ip | grep "64"; 
       then 
-       echo "$ip : Is alive" >> $logsPath
-      elif [ $? -eq 1 ];then 
-       echo "$ip : Request timed out" | tee -a $logsPath | tee -a $logsPath1
-      elif [ $? -eq 2 ];then 
-       echo "$ip : Destination host unreachable" | tee -a $logsPath | tee -a $logsPath2
-      fi 
+          echo "$ip : Is alive" >> $logsPath
+      elif ping -c 1 $ip | grep "+";then 
+          echo "$ip : Destination host unreachable" | tee -a $logsPath | tee -a $logsPath2
+      elif ping -c 1 $ip | grep "100";then 
+          echo "$ip : Request timed out" | tee -a $logsPath | tee -a $logsPath1
+      fi
    )& ##### end of sub shell who allowed us to execute parallel pings (thread)
 done 
 wait
-echo "&#9888;&#65039; One or more machines did not answered to the last ping scan. <br>-> <b> Request timed out </b>" | mail -s "$(echo -e "Connectivity loose report - connectivityGuardian \nContent-Type: text/html")" -A $logsPath1 $email
-#echo "&#9888;&#65039; One or more machines did not answered to the last ping scan. <br>-> <b> Destination host unreachable </b> " | mail -s "$(echo -e "Perte de connectivité - VM Lab \nContent-Type: text/html")" -A $logsPath2 $email
+# echo "&#9888;&#65039; One or more machines did not answered to the last ping scan. <br>-> <b> Request timed out </b>" | mail -s "$(echo -e "Connectivity loose report - connectivityGuardian \nContent-Type: text/html")" -A $logsPath1 $email
+# echo "&#9888;&#65039; One or more machines did not answered to the last ping scan. <br>-> <b> Destination host unreachable </b> " | mail -s "$(echo -e "Perte de connectivité - VM Lab \nContent-Type: text/html")" -A $logsPath2 $email
 
