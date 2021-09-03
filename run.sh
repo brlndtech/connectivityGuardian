bcffb6cb9790ce4a2669220699d2b0e73a092a83
Brlndtech <brlndtech@gmail.com>
2021-09-03 11:13:54 +0200

    remote mail api link works, with the temp file tmp.txt that store unreachable dns/ip

--- run.sh
+++ run.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 logsPath="/etc/connectivityGuardian/output-all.txt" ; logsPath1="/etc/connectivityGuardian/output-request-timed-out.txt" ; logsPath2="/etc/connectivityGuardian/output-host-is-unreachable.txt" ; file="/etc/connectivityGuardian/ip-dns.txt"
-email="gsb@yopmail.com"
+email="vmsafeguard@yopmail.com"
 d="$(date +%d-%m-%Y-%H-%M)"
 lines=$(cat $file)
+tmp="tmp.txt"
+touch $tmp
 for ip in $lines ; 
 do (  ##### sub shell who allow us to execute parallel pings (thread)
-      if ping -c 1 $ip | grep "64"; 
-      then 
+      if ping -c 1 $ip | grep "64"; then 
           echo "$ip : Is alive" >> $logsPath
-      elif ping -c 1 $ip | grep "+";then 
+      elif ping -c 1 $ip | grep "+"; then 
           echo "$ip : Destination host unreachable" | tee -a $logsPath | tee -a $logsPath2
-      elif ping -c 1 $ip | grep "100";then 
+          echo "$ip" | tee -a $tmp
+      elif ping -c 1 $ip | grep "100"; then 
           echo "$ip : Request timed out" | tee -a $logsPath | tee -a $logsPath1
+          echo "$ip" | tee -a $tmp      
       fi
    )& ##### end of sub shell who allowed us to execute parallel pings (thread)
-done 
+done
 wait
-# echo "&#9888;&#65039; One or more machines did not answered to the last ping scan. <br>-> <b> Request timed out </b>" | mail -s "$(echo -e "Connectivity loose report - connectivityGuardian \nContent-Type: text/html")" -A $logsPath1 $email
-# echo "&#9888;&#65039; One or more machines did not answered to the last ping scan. <br>-> <b> Destination host unreachable </b> " | mail -s "$(echo -e "Perte de connectivité - VM Lab \nContent-Type: text/html")" -A $logsPath2 $email
-
+arr=()
+while IFS= read -r line; do
+   arr+=("$line,")
+done <$tmp
+h=$(echo ${arr[@]})
+curl -d "test=coguardian&ip=$h&adminEmail=$email" -X POST https://le-guide-du-sysops.fr/vmsafeguard-mail-api/mail.php
+rm $tmp
